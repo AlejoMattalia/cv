04b9faa7710d8d361bbcfefd6aa0f8ed38a9046a
AlejoMattalia <alejoomattalia@gmail.com>
2025-05-31 03:49:48 -0300

    Agregamos Inglés

---
+++ main.js
@@ -0,0 +1,195 @@
+const translations = {
+  en: {
+    buttonEn: "English",
+    buttonEs: "Spanish",
+    title: "Frontend Developer",
+    date: "April 27, 2004",
+    description:
+      "Frontend Developer with a background in programming and experience in building web applications using React.js and Next.js. I have worked on both freelance and team projects, where I developed problem-solving skills and the ability to create intuitive user interfaces. I also have knowledge of Node.js and Nest.js to complement my frontend development. I enjoy staying up to date with the latest technology trends.",
+    titleEducation: "Education",
+    dateEducation: "Mar 2022 - Dec 2023",
+    titleTeclab: "Higher Technician in Programming",
+    descriptionTeclab:
+      "I learned to program in JavaScript, Java, and Python through a 100% online degree program, with practical projects that allowed me to apply my knowledge to real-world situations, always supported by tutors and instructors.",
+    titleExperience: "Experience",
+    dateMDabroad: "jun 2025 - present",
+    titleMDabroad: "Frontend Software Engineer",
+    textMDabroad: "Soon",
+    titleByron: "Frontend Developer",
+    textByron:
+      "I developed the landing page and application for OrdenApp, a platform for restaurants that includes features such as menu management with categories and suggestions, a digital menu accessible via QR codes, payment integration with Mercado Pago, sales analysis with detailed reports, and table and staff management. I used technologies like React.js, TypeScript, and MUI, highlighting my teamwork skills, effective communication, and leadership in coordinating the project.",
+    dateBeCode: "apr 2024 - nov 2024",
+    titleBeCode: "FullStack Developer",
+    textBeCode:
+      "I worked on the frontend development of web applications and the backend of mobile applications, using technologies such as React and Node.js. I implemented the Scrum methodology, which allowed me to collaborate effectively within a team and manage a group under my responsibility. Additionally, I focused on training team members and presenting projects to clients.",
+    dateFreelance: "jan 2023 - present",
+    titleFreelance: "Developer Freelance",
+    subtitleFreelance: "FREELANCE, ARGENTINA",
+    textFreelance:
+      "In my freelance work, I have developed web applications using React.js and Node.js, working directly with clients to define and meet their requirements. I have handled everything from creating user interfaces to integrating payment systems and WhatsApp chatbots, adapting to new technologies and challenges in each project.",
+    titleCourse: "Course",
+    dateCoder: "Jan 2023 - May 2023",
+    titleCoder: "Frontend Developer Program with React js",
+    textCoder:
+      "I learned to build web applications using HTML, CSS, JavaScript, and React. The course was 100% online and hands-on, with constant support from mentors who guided me throughout the process, helping me apply what I learned in a direct and practical way.",
+    dateBootcamp: "aug 2023 - dec 2023",
+    titleBootcamp: "Backend Development",
+    subtitleBootcamp: "WEBEXPERTO COMPANY BOOTCAMP",
+    textBootcamp:
+      "I completed the WebExperto bootcamp, where I learned backend development with Ruby on Rails and MySQL. The program was intensive and hands-on, focused on real-world projects and supported by mentors, which prepared me for the job market as a web developer.",
+    titleTechnologies: "Technologies",
+    titleSkills: "Skills",
+    skillOne: "Frontend Development",
+    skillTwo: "Backend Development",
+    skillThree: "Databases",
+    skillFour: "Agile methodologies",
+    skillFive: "Teamwork",
+    skillSix: "Leadership",
+    skillSeven: "Problem-solving",
+    skillEight: "Project presentation",
+    skillNine: "Adaptability and rapid learning",
+    skillTen: "Optimization and scalability",
+    titleLanguages: "Languages",
+    languageOne: "Spanish: Native",
+    languageTwo: "English: Intermediate",
+  },
+  es: {
+    buttonEn: "Inglés",
+    buttonEs: "Español",
+    title: "Desarrollador Frontend",
+    date: "27 de abril de 2004",
+    description:
+      "Desarrollador Frontend con formación en programación y experiencia en la creación de aplicaciones web con React.js, Next.js. Trabajé en proyectos freelance y en equipo, donde he desarrollado habilidades en resolución de problemas y construcción de interfaces intuitivas. También tengo conocimientos en Node.js y Nest.js para complementar el desarrollo frontend. Me gusta mantenerme al día con las tendencias tecnológicas.",
+    titleEducation: "Formación",
+    dateEducation: "mar 2022 - dic 2023",
+    titleTeclab: "Técnico Superior en Programación",
+    descriptionTeclab:
+      "Aprendí a programar en JavaScript, Java y Python en una carrera 100% online, con proyectos prácticos que me permitieron aplicar los conocimientos en situaciones reales, siempre con el apoyo de tutores y profesores.",
+    titleExperience: "Experiencia",
+    dateMDabroad: "jun 2025 - presente",
+    titleMDabroad: "Ingeniero de software Frontend",
+    textMDabroad: "Próximamente ",
+    titleByron: "Desarrollador Frontend",
+    textByron:
+      "Desarrollé la landing page y la aplicación de OrdenApp, una plataforma para restaurantes que incluye funcionalidades como gestión de menús con categorías y sugerencias, carta digital accesible mediante códigos QR, integración de pagos con Mercado Pago, análisis de ventas con reportes detallados, y gestión de mesas y organización del personal. Utilicé tecnologías como React.js, TypeScript, y MUI, destacando mi capacidad de trabajo en equipo, comunicación efectiva, y liderazgo en la coordinación del proyecto.",
+    dateBeCode: "abr 2024 - nov 2024",
+    titleBeCode: "Desarrollador FullStack",
+    textBeCode:
+      "Trabajé en el desarrollo del frontend de aplicaciones web y del backend de aplicaciones móviles, utilizando tecnologías como React y Node.js. Implementé la metodología Scrum, lo que me permitió trabajar de manera colaborativa en equipo y gestionar un grupo a mi cargo. Además, me enfoqué en capacitar a los miembros del equipo y presentar proyectos a clientes.",
+    dateFreelance: "ene 2023 - presente",
+    titleFreelance: "Desarrollador Freelance",
+    subtitleFreelance: "AUTÓNOMO, ARGENTINA",
+    textFreelance:
+      "En mis trabajos freelance, he desarrollado aplicaciones web usando React.j y Node.js, trabajando directamente con clientes para definir y cumplir sus requerimientos. He manejado desde la creación de interfaces hasta la integración de pagos y chatbots de whatsapp, adaptándome a nuevas tecnologías y desafíos en cada proyecto.",
+    titleCourse: "Cursos",
+    dateCoder: "ene 2023 - may 2023",
+    titleCoder: "Carrera de Desarrollador Frontend con React js",
+    textCoder:
+      "Aprendí a crear aplicaciones web usando HTML, CSS, JavaScript y React. El curso fue 100% online y práctico, con el acompañamiento constante de mentores que me guiaron en todo momento, ayudándome a aplicar lo aprendido de forma directa y concreta.",
+    dateBootcamp: "ago 2023 - dic 2023",
+    titleBootcamp: "Desarrollo Backend",
+    subtitleBootcamp: "BOOTCAMP EMPRESA WEBEXPERTO",
+    textBootcamp:
+      "Hice el bootcamp de WebExperto, donde aprendí desarrollo backend con Ruby on Rails y MySQL. El programa fue intensivo y práctico, enfocado en proyectos reales y con el apoyo de mentores, lo que me preparó para el mercado laboral como desarrollador web.",
+    titleTechnologies: "Tecnologías",
+    titleSkills: "Aptitudes",
+    skillOne: "Desarrollo Frontend",
+    skillTwo: "Desarrollo Backend",
+    skillThree: "Bases de Datos",
+    skillFour: "Metodologías agiles",
+    skillFive: "Trabajo en equipo",
+    skillSix: "Liderazgo",
+    skillSeven: "Resolución de problemas",
+    skillEight: "Presentación de proyectos",
+    skillNine: "Adaptabilidad y aprendizaje rápido",
+    skillTen: "Optimización y escalabilidad",
+    titleLanguages: "Idiomas",
+    languageOne: "Español: Nativo",
+    languageTwo: "Inglés: Intermedio",
+  },
+};
+
+// Capturamos todos los botones
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    const lang = button.dataset.lang; // "en" o "es"
+    const selectedText = translations[lang];
+
+    // Cambiamos el contenido del DOM
+
+    document.getElementById("buttonEn").textContent = selectedText.buttonEn;
+    document.getElementById("buttonEs").textContent = selectedText.buttonEs;
+    document.getElementById("title").textContent = selectedText.title;
+    document.getElementById("date").textContent = selectedText.date;
+    document.getElementById("description").textContent =
+      selectedText.description;
+    document.getElementById("titleEducation").textContent =
+      selectedText.titleEducation;
+    document.getElementById("dateEducation").textContent =
+      selectedText.dateEducation;
+    document.getElementById("titleTeclab").textContent =
+      selectedText.titleTeclab;
+    document.getElementById("descriptionTeclab").textContent =
+      selectedText.descriptionTeclab;
+    document.getElementById("titleExperience").textContent =
+      selectedText.titleExperience;
+    document.getElementById("dateMDabroad").textContent =
+      selectedText.dateMDabroad;
+    document.getElementById("titleMDabroad").textContent =
+      selectedText.titleMDabroad;
+    document.getElementById("textMDabroad").textContent =
+      selectedText.textMDabroad;
+    document.getElementById("titleByron").textContent = selectedText.titleByron;
+    document.getElementById("textByron").textContent = selectedText.textByron;
+    document.getElementById("dateBeCode").textContent = selectedText.dateBeCode;
+    document.getElementById("titleBeCode").textContent =
+      selectedText.titleBeCode;
+    document.getElementById("textBeCode").textContent = selectedText.textBeCode;
+    document.getElementById("dateFreelance").textContent =
+      selectedText.dateFreelance;
+    document.getElementById("titleFreelance").textContent =
+      selectedText.titleFreelance;
+    document.getElementById("subtitleFreelance").textContent =
+      selectedText.subtitleFreelance;
+    document.getElementById("textFreelance").textContent =
+      selectedText.textFreelance;
+    document.getElementById("titleCourse").textContent =
+      selectedText.titleCourse;
+    document.getElementById("dateCoder").textContent = selectedText.dateCoder;
+    document.getElementById("titleCoder").textContent = selectedText.titleCoder;
+    document.getElementById("textCoder").textContent = selectedText.textCoder;
+    document.getElementById("dateBootcamp").textContent =
+      selectedText.dateBootcamp;
+    document.getElementById("titleBootcamp").textContent =
+      selectedText.titleBootcamp;
+    document.getElementById("subtitleBootcamp").textContent =
+      selectedText.subtitleBootcamp;
+    document.getElementById("textBootcamp").textContent =
+      selectedText.textBootcamp;
+    document.getElementById("titleTechnologies").textContent =
+      selectedText.titleTechnologies;
+    document.getElementById("titleSkills").textContent =
+      selectedText.titleSkills;
+    document.getElementById("skillOne").textContent = selectedText.skillOne;
+    document.getElementById("skillTwo").textContent = selectedText.skillTwo;
+    document.getElementById("skillThree").textContent = selectedText.skillThree;
+    document.getElementById("skillFour").textContent = selectedText.skillFour;
+    document.getElementById("skillFive").textContent = selectedText.skillFive;
+    document.getElementById("skillSix").textContent = selectedText.skillSix;
+    document.getElementById("skillSeven").textContent = selectedText.skillSeven;
+    document.getElementById("skillEight").textContent = selectedText.skillEight;
+    document.getElementById("skillNine").textContent = selectedText.skillNine;
+    document.getElementById("skillTen").textContent = selectedText.skillTen;
+    document.getElementById("titleLanguages").textContent =
+      selectedText.titleLanguages;
+    document.getElementById("languageOne").textContent =
+      selectedText.languageOne;
+    document.getElementById("languageTwo").textContent =
+      selectedText.languageTwo;
+  });
+});
